e54dd70936d4ba192506edcfc34fb79b3236ce4a
KrackerOs <kleberisaac900@gmail.com>
2025-11-26 09:55:05 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,51 @@
+const { useState } = require("react");
+
 function App(){
+  const [task, setTask] = useState ([
+  {
+    id:1,
+    title:"Armoçar macarão com sarcixa",
+    description:"Comer sarcixa com macarão pra ficar gordo",
+    isCompleteted:false
+    
+  }
+
+  {
+    id:2,
+    title: "Calistenia",
+    description: "2X12 Flexões de joelho, 3x12 levantamento de peso",
+    isCompleteted:false
+  }
+
+  {
+    id:3,
+    title:"Lazer",
+    description:"Jogatina",
+    isCompleteted:false
+  }
+
+  ]);
+       return(
+        <h1>Gerenciando Tarefas</h1>
+       );
+
+
+
+
+       
+}
+
+
+export default App;
+
+
+
+
+
+
+
+
+/*function App(){
   const [message, setMessage] = useState('Olá Mundo');
   return(
     <div>
